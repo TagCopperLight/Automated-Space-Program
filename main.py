14f06b60c489ac246ed839e0f21d519283ba4290
TagCopperLight <tagcopperlight@gmail.com>
2022-05-20 09:53:39 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 import time
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import time
 
 pygame.init()
 pygame.font.init()
@@ -12,29 +15,31 @@ class PID:
         self.__Ki = ki
         self.__Kd = kd
 
+        self.__total_error = 0
         self.__previous_error = 0
 
     def update(self, error):
         error = -error
         p = error
-        i = self.__previous_error + error
+        i = self.__total_error + error
         d = error - self.__previous_error
         self.__previous_error = error
+        self.__total_error += error
         return self.__Kp * p + self.__Ki * i + self.__Kd * d
 
 class Fusee:
     def __init__(self):
         self.fusee = pygame.transform.scale(pygame.image.load('ratio.png').convert_alpha(), (86/5, 638/5))
-        self.position = pygame.Vector2(0, 50)
+        self.position = pygame.Vector2(0, 400)
         self.velocity = pygame.Vector2()
         self.acceleration = pygame.Vector2()
 
         self.gravity = pygame.Vector2(0, 0.05)
-        self.max_thrust = pygame.Vector2(0, -0.1)
+        self.max_thrust = pygame.Vector2(0, -0.062625489)
         self.drag = pygame.Vector2(0, 0)
 
-        self.thrust = 49
-        self.mass = 1
+        self.thrust = 80
+        self.fuel = 1
 
     def update(self):
         if self.velocity == pygame.Vector2():
@@ -42,31 +47,35 @@ class Fusee:
         else:
             self.drag = -self.velocity.normalize() * (1/2 * 1.2 * self.velocity.magnitude_squared() * 0.47 * 0.01) / 2
 
-        if self.thrust < 0:
+        if self.thrust < 0 or self.fuel <= 0:
             self.thrust = 0
         elif self.thrust > 100:
             self.thrust = 100
 
-        self.acceleration += self.gravity + self.max_thrust * (self.thrust / 100) + self.drag
+        self.acceleration += self.gravity + (self.max_thrust * (self.thrust / 100) + self.drag) / ((1 + self.fuel) * 0.3)
 
         self.velocity += self.acceleration
         self.acceleration = pygame.Vector2()
 
         self.position += self.velocity
 
-        if self.mass - 0.0005 <= 0:
-            self.mass = 0
-        else:
-            self.mass -= 0.0001
+        self.fuel -= 0.00001 * self.thrust / 100
 
 
 boom = pygame.image.load('boom.png').convert_alpha()
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
 fusee = Fusee()
-pid = PID(.3, 1, 2)
+pid = PID(1, 0.0001, 750)
 
 desired_altitude = 200
+
+beninging = time.time_ns()
+x_pos, y_pos = [], []
+x_desired, y_desired = [], []
+plt.figure()
+
+pid_c = True
 
 running = True
 while running:
@@ -75,9 +84,15 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                desired_altitude += 5
+                desired_altitude += 50
             if event.key == pygame.K_DOWN:
-                desired_altitude -= 5
+                desired_altitude -= 50
+            if event.key == pygame.K_s:
+                fusee.thrust -= 1
+            if event.key == pygame.K_z:
+                fusee.thrust += 1
+            if event.key == pygame.K_SPACE:
+                pid_c = not pid_c
 
     screen.fill((0, 0, 0))
 
@@ -85,17 +100,28 @@ while running:
         screen.blit(boom, (screen.get_width() // 2, screen.get_height() // 2))
         running = False
 
-    fusee.thrust = pid.update(desired_altitude - fusee.position.y)
+    if pid_c:
+        fusee.thrust = pid.update(desired_altitude - fusee.position.y)
     fusee.update()
 
     text_surface = my_font.render(str(fusee.thrust), False, (255, 255, 255))
     text_surface2 = my_font.render(str(desired_altitude) + "  " + str(fusee.position.y), False, (255, 255, 255))
-    text_surface3 = my_font.render(str(fusee.mass), False, (255, 255, 255))
+    text_surface3 = my_font.render(str(fusee.fuel), False, (255, 255, 255))
     screen.blit(text_surface, (0, 0))
     screen.blit(text_surface2, (0, 30))
     screen.blit(text_surface3, (0, 60))
 
     screen.blit(fusee.fusee, (screen.get_width() // 2, fusee.position.y))
     pygame.display.update()
-    print()
-    # time.sleep(0.5)
+
+    x_pos.append(time.time_ns() - beninging)
+    y_pos.append(-fusee.position.y)
+
+    x_desired.append(time.time_ns() - beninging)
+    y_desired.append(-desired_altitude)
+
+
+plt.plot(x_pos, y_pos, 'r')
+plt.plot(x_desired, y_desired, 'g')
+
+plt.show()
