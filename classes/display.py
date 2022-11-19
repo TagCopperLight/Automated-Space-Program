2787c50a774d516f73f0a24a1c84d077dc496058
TagCopperLight <tagcopperlight@gmail.com>
2022-11-19 15:45:23 +0100

    Display update

--- classes/display.py
+++ classes/display.py
@@ -7,18 +7,18 @@ from utils.utils import convert_position
 class Display:
     def __init__(self, debug=False):
         self.debug = debug
-        self.FONT = font.SysFont('Comic Sans MS', 30)
+        self.FONT = font.SysFont('Comic Sans MS', 20)
 
     def update(self, screen_size, rocket):
         surface = Surface(screen_size, SRCALPHA)
 
         if self.debug:
-            velocity_text = self.FONT.render(f'Velocity : {rocket.velocity.y:.3f} m/s', False, Colors.WHITE.value)
-            position_text = self.FONT.render(f'Altitude : {rocket.position.y:.3f} m', False, Colors.WHITE.value)
-            term_vel_text = self.FONT.render(f'Terminal velocity : {rocket.term_velocity:.3f} m/s', False, Colors.WHITE.value)
-            thrust_text = self.FONT.render(f'Thrust : {rocket.thrust * 100:.3f} %', False, Colors.WHITE.value)
-            fuel_text = self.FONT.render(f'Fuel : {rocket.fuel / rocket.fuel_mass * 100:.3f} %', False, Colors.WHITE.value)
-            mass_text = self.FONT.render(f'Mass : {rocket.mass:.3f} kg', False, Colors.WHITE.value)
+            velocity_text = self.FONT.render(f'Velocity : {rocket.velocity.y:.3f} m/s', True, Colors.BLACK.value)
+            position_text = self.FONT.render(f'Altitude : {rocket.position.y:.3f} m', True, Colors.BLACK.value)
+            term_vel_text = self.FONT.render(f'Terminal velocity : {rocket.term_velocity:.3f} m/s', True, Colors.BLACK.value)
+            thrust_text = self.FONT.render(f'Thrust : {rocket.thrust * 100:.3f} %', True, Colors.BLACK.value)
+            fuel_text = self.FONT.render(f'Fuel : {rocket.fuel / rocket.fuel_mass * 100:.3f} %', True, Colors.BLACK.value)
+            mass_text = self.FONT.render(f'Mass : {rocket.mass:.3f} kg', True, Colors.BLACK.value)
             surface.blit(velocity_text, (10, 10))
             surface.blit(position_text, (10, 40))
             surface.blit(term_vel_text, (10, 70))
